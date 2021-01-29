19de05a4e480ad5950dece54c7cd9fa3d6bec9c3
John-Denver <denverbogonko78@gmail.com>
2021-01-29 21:05:29 +0300

    Challenge

--- challenges/week_1/bus_fare_challenge.py
+++ challenges/week_1/bus_fare_challenge.py
@@ -1,1 +1,32 @@
-# WRITE YOUR CODE SOLUTION HERE
+import datetime
+
+date = datetime.date.today()
+
+weekDays = ("Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday")
+
+dayAsString = datetime.date.today()
+asweek = dayAsString.weekday()
+day = weekDays[asweek]
+
+print('Date: ' +str(date))
+print('Day: ' +str(day))
+
+def ans():
+    if day == "Monday":
+        print('Fare: 100')
+    elif day == "Tuesday":
+        print('Fare: 100')
+    elif day == "Wednesday":
+        print('Fare: 100')
+    elif day == "Thursday":
+        print('Fare: 100')
+    elif day == "Friday":
+        print('Fare: 100')
+
+    elif day == "Saturday":
+        print('Fare: 60')
+    else:
+        print('Fare: 80')
+
+ans()
+
